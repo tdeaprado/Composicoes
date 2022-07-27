c12636e68979f8aa9beff1ecfe272865d55fff73
Thiago Prado <thiago_cpv92@hotmail.com>
2022-07-27 18:57:33 -0300

    modificando classe trabalhador

--- src/Entities/Trabalhador.java
+++ src/Entities/Trabalhador.java
@@ -8,4 +8,45 @@ public class Trabalhador {
     private WorkerLevel nivel;
     private Double salarioBase;
     
+    public Trabalhador ()
+    {
+    	
+    }
+    
+	public Trabalhador(String nome, WorkerLevel nivel, Double salarioBase) {
+		super();
+		this.nome = nome;
+		this.nivel = nivel;
+		this.salarioBase = salarioBase;
+	}
+    
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public WorkerLevel getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(WorkerLevel nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+
+    
+    
 }
