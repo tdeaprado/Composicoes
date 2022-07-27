e5dea5346495ec8cf07f42f979f8f7d0c49ad658
Thiago Prado <thiago_cpv92@hotmail.com>
2022-07-27 19:16:00 -0300

    Add relacionamentos

--- src/Entities/Trabalhador.java
+++ src/Entities/Trabalhador.java
@@ -1,5 +1,7 @@
 package Entities;
 
+import java.util.List;
+
 import enums.WorkerLevel;
 
 public class Trabalhador {
@@ -8,44 +10,89 @@ public class Trabalhador {
     private WorkerLevel nivel;
     private Double salarioBase;
     
+    private Departamento departamento;
+    private List <ContratoHora> contratos;
+    
+    
+    
     public Trabalhador ()
     {
     	
     }
-    
-	public Trabalhador(String nome, WorkerLevel nivel, Double salarioBase) {
-		super();
+
+
+
+	public Trabalhador(String nome, WorkerLevel nivel, Double salarioBase, Departamento departamento,
+			List<ContratoHora> contratos) {
 		this.nome = nome;
 		this.nivel = nivel;
 		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+		this.contratos = contratos;
 	}
-    
-	
+
+
 
 	public String getNome() {
 		return nome;
 	}
 
+
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
 
 	public WorkerLevel getNivel() {
 		return nivel;
 	}
 
+
+
 	public void setNivel(WorkerLevel nivel) {
 		this.nivel = nivel;
 	}
+
+
 
 	public Double getSalarioBase() {
 		return salarioBase;
 	}
 
+
+
 	public void setSalarioBase(Double salarioBase) {
 		this.salarioBase = salarioBase;
 	}
 
+
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+
+
+	public List<ContratoHora> getContratos() {
+		return contratos;
+	}
+
+
+
+	public void setContratos(List<ContratoHora> contratos) {
+		this.contratos = contratos;
+	}
+    
+
+    
 
     
     
